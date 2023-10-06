e09c841140b87a852ebfd2e8ee3071fd8a83bcdf
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-10-07 01:45:50 +0600

    service card added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,46 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
+import { useLoaderData } from "react-router-dom";
+import ServicesCard from "./ServicesCard";
 
 const Home = () => {
-    const bckImg = {
-        backgroundImage: 'url("https://i.ibb.co/DpD2JM9/background.jpg")'
-    }
-  
-  return (
-    <div className="mt-5">
-      <Navbar></Navbar>
-      {/* Banner Section */}
-      <div style={bckImg}>
-        this is home
+  const services = useLoaderData();
+  const bckImg = {
+    backgroundImage:
+      'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.5)), url("https://i.ibb.co/DpD2JM9/background.jpg")',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    position: "relative",
+  };
 
+  return (
+    <div>
+      <div className="h-screen" style={bckImg}>
+        <div>
+          <Navbar></Navbar>
+          {/* Banner Section */}
+          <div className="max-w-7xl mx-auto mt-10 text-white">
+            <div className="text-left flex justify-normal items-left pt-16">
+              <h2 className="text-9xl font-semibold">
+                Mindfulness <br />
+                <p className="text-7xl font-medium">Yoga & Fitness</p>
+                <p className="text-3xl text-orange-200 pt-5">
+                  Yoga & Fitness is a practice that connects the body, breath,
+                  and mind. It uses physical postures, breathing exercises, and
+                  meditation to improve overall health. Yoga was developed as a
+                  spiritual practice thousands of years ago...
+                </p>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-5xl font-semibold text-center py-10">Services</h1>
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {services.map((aService) => (
+          <ServicesCard services={aService}></ServicesCard>
+        ))}
       </div>
     </div>
   );
---
+++ src/pages/ServicesCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+
+const ServicesCard = ({services}) => {
+
+    const {name, image, short_description, price} = services;
+
+    return (
+        <div className='max-w-7xl mx-auto'>
+            
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
+                {
+                    <div class="relative flex w-96 flex-col rounded-3xl bg-[#525150] text-orange-200 bg-clip-border shadow-lg shadow-[#424241]">
+                    <div class="relative mx-4 -mt-6 h-56 overflow-hidden  bg-blue-gray-500 bg-clip-border text-white  ">
+                      <img
+                        src={image}
+                        alt="img-blur-shadow"
+                        layout="fill"
+                      />
+                    </div>
+                    <div class="p-6">
+                      <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                        {name}
+                      </h5>
+                      <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                        {short_description}
+                      </p>
+                      <p className='text-xl '> $ {price}</p>
+
+                    </div>
+                    <div class="p-6 pt-0">
+                      <button
+                        class="select-none rounded-lg bg-orange-400 py-3 px-6 text-center align-middle font-sans text-xs font-bold  text-white shadow-md shadow-orange-500/40 transition-all hover:shadow-lg hover:shadow-orange-800 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        type="button"
+                        data-ripple-light="true"
+                      >
+                        Details
+                      </button>
+                    </div>
+                  </div>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ServicesCard;
