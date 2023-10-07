de062f05276097b799718daaec3033a5d5e68337
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-10-07 18:15:18 +0600

    firebase environment added

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCW2dxE4Gmyv4_WieGGpXJcno1zU7U0a7I",
-  authDomain: "event-management-8e486.firebaseapp.com",
-  projectId: "event-management-8e486",
-  storageBucket: "event-management-8e486.appspot.com",
-  messagingSenderId: "223708679580",
-  appId: "1:223708679580:web:13a8457de802a6b4240c59"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET, 
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID, 
 };
 
 // Initialize Firebase
