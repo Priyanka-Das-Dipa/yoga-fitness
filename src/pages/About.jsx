fe8e499b16472f63a35b29093dbdff684ae63e1d
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-10-09 17:13:54 +0600

    quiz file added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         About Us
       </h1>
       <section className="grid grid-cols-2 justify-center items-center gap-5">
-        <div data-aos="zoom-in-right" data-aos-duration="2000" className="space-y-3">
+        <div data-aos="zoom-in-left" data-aos-duration="2000" className="space-y-3">
           <h1 className="text-5xl font-semibold">Yoga & <span className="text-orange-300">Fitness Center</span> </h1>
           <p className="text-xl font-medium ">We are a gym that committed to helping people reach there fitness goal. We often a verity of there programs and services to fit your needs, whether you are a experienced athlete. <br /> We believe that everyone should have access to the benefits of exercise make it happen.</p>
           <h2 className="text-3xl font-semibold">Why should people choose <span className="text-orange-300">Yoga & Fitness</span> services</h2>
@@ -21,7 +21,7 @@ const About = () => {
             <p>There are many fitness classes that offered during off-peak hours, such as early morning or late evening.</p>
           </ol>
         </div>
-        <div data-aos="zoom-in-left" data-aos-duration="2000">
+        <div data-aos="zoom-in-right" data-aos-duration="2000">
           <img className="shadow-lg shadow-black rounded-lg" src="https://i.ibb.co/k4rnRbP/man-women-doing-crunches.jpg" alt="" />
         </div>
       </section>
